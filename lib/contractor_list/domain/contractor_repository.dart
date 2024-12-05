ead05b1fa90c5da8d7e157130a08bb7b918f288f
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2024-12-05 00:13:12 -0600

    checked for errors

--- lib/contractor_list/domain/contractor_repository.dart
+++ lib/contractor_list/domain/contractor_repository.dart
@@ -5,41 +5,42 @@ import 'package:rate_my_contractor/contractor_list/data/contractor_data_remote_p
 import 'package:rate_my_contractor/contractor_list/data/models/license_dto.dart';
 import 'package:rate_my_contractor/contractor_list/domain/models/contractor.dart';
 import '../data/models/contractor_dto.dart';
+
 //have a try catch here and throw dont return an empty list
 class ContractorRepository {
   const ContractorRepository(this._contractorDataRemoteProvider);
   //getting the data provider
   final ContractorDataRemoteProvider _contractorDataRemoteProvider;
 
-  Future<List<Contractor>> getContractors(String query) async { 
-    final List<ContractorDto> dataSetContractors = await _contractorDataRemoteProvider.getContractors(query); 
+  Future<List<Contractor>> getContractors(String query) async {
+    final List<ContractorDto> dataSetContractors =
+        await _contractorDataRemoteProvider.getContractors(query);
     final List<String> contractorIds = [];
     //List<Contractor> listOfContractors = [];
-    
+
     for (var dsContractor in dataSetContractors) {
-       contractorIds.add(dsContractor.id);
+      contractorIds.add(dsContractor.id);
     }
 
-    List<LicenseDto> dataSetLicenses = await _contractorDataRemoteProvider.getLicenses(contractorIds);
+    List<LicenseDto> dataSetLicenses =
+        await _contractorDataRemoteProvider.getLicenses(contractorIds);
 
-    List<Contractor> listOfContractors = dataSetContractors.map((contractor) { 
-        List<LicenseDto> licensesMatch = dataSetLicenses 
-        .where((license) => license.contractorId == contractor.id)
-        .toList();
+    List<Contractor> listOfContractors = dataSetContractors.map((contractor) {
+      List<LicenseDto> licensesMatch = dataSetLicenses
+          .where((license) => license.contractorId == contractor.id)
+          .toList();
 
-        return Contractor( 
-          id: contractor.id, 
-          companyName: contractor.companyname, 
+      return Contractor(
+          id: contractor.id,
+          companyName: contractor.companyname,
           address: contractor.address,
           ownerName: contractor.owner,
           phone: contractor.phone,
           email: contractor.email,
           licenses: licensesMatch,
-          tags: licensesMatch.map((licenses) => licenses.licenseType).toList()
-        );
-      }
-    ).toList();
-  
+          tags: licensesMatch.map((licenses) => licenses.licenseType).toList());
+    }).toList();
+
     return listOfContractors;
   }
 }
