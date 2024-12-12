fedfa2a5bfbc8b450a996a033c0278fcb5ad5326
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2024-12-12 01:33:35 -0600

    added bloc files for signup

--- lib/authentication/data/user_data_provider.dart
+++ lib/authentication/data/user_data_provider.dart
@@ -6,11 +6,17 @@ class UserDataProvider {
   final SupabaseClient _supabaseClient;
 
   /// Sign in
+  /// doesnt return a value pass email and password and call supaabse and sighin with password
   Future<void> signIn(String email, String password) async {
     await _supabaseClient.auth.signInWithPassword(
       email: email,
       password: password,
     );
+  }
+
+  ///using _supabaseClient.auth. we are now able to retrieve the user information
+  User? userInformation() {
+    return _supabaseClient.auth.currentUser;
   }
 
   /// Check Auth
--- lib/authentication/domain/authentication_repository.dart
+++ lib/authentication/domain/authentication_repository.dart
@@ -2,18 +2,26 @@ import 'package:rate_my_contractor/authentication/data/user_data_provider.dart';
 
 enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 
+/*
+authenticated - the user is logged in
+unauthenticated - the user isnt logged in 
+*/
 class AuthenticationRepository {
   const AuthenticationRepository(this._userDataProvider);
 
   final UserDataProvider _userDataProvider;
 
   Future<void> signIn(String email, String password) async {
-    await _userDataProvider.signIn(email, password);
+    //future<void> means it wont return nothing just waiting for the call
+    await _userDataProvider.signIn(
+        email, password); //sending email and password to supabase
   }
 
   Stream<AuthenticationStatus> get status =>
       _userDataProvider.status.map((authState) {
         if (authState.session != null) {
+          //checks if session is active
+          _userDataProvider.userInformation();
           return AuthenticationStatus.authenticated;
         } else {
           return AuthenticationStatus.unauthenticated;
