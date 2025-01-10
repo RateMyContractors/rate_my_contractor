e32aa31eddca6f6c270c35a8fbb75ddab882c9fb
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-01-10 15:05:25 -0600

    added drivers to git ignore

--- test/contractor_page_test.dart
+++ test/contractor_page_test.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rate_my_contractor/contractor_page.dart';
 import 'package:rate_my_contractor/models/contractor.dart';
-import 'package:rate_my_contractor/results_page.dart';
 
-void main(){ 
+void main() {
   var testContractor = const Contractor(
     id: 4,
     companyName: "Handy Home Repairs",
@@ -12,25 +11,26 @@ void main(){
     image: "/placeholder.svg?height=100&width=100",
     rating: 4.8,
     tags: ["General Repairs", "Carpentry", "Painting"],
-    phone: '', email: '',
+    phone: '',
+    email: '',
     aboutUs: 'blah blah blah',
   );
 
-  testWidgets('Contractor card appears on the page', (tester)async{
+  testWidgets('Contractor card appears on the page', (tester) async {
     await tester.pumpWidget(MaterialApp(
       home: ContractorPage(contractor: testContractor),
     ));
     expect(find.text("Handy Home Repairs"), findsOneWidget);
   });
 
-  testWidgets('About us appears on the page', (tester)async{
+  testWidgets('About us appears on the page', (tester) async {
     await tester.pumpWidget(MaterialApp(
       home: ContractorPage(contractor: testContractor),
     ));
     expect(find.text("About Us\n"), findsOneWidget);
   });
 
-  testWidgets('Portfolio appears on the page', (tester)async{
+  testWidgets('Portfolio appears on the page', (tester) async {
     await tester.pumpWidget(MaterialApp(
       home: ContractorPage(contractor: testContractor),
     ));
