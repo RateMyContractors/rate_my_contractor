f5cb98520d7bde514e757509697bc5094a7e24bb
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-01-16 21:05:58 -0600

    fixed about us

--- lib/widgets/about_widget.dart
+++ lib/widgets/about_widget.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
+
 class AboutUsWidget extends StatelessWidget {
   final String aboutUs;
   const AboutUsWidget({
-    super.key, 
+    super.key,
     required this.aboutUs,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey, width: 1.0),
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 5.0,
-              offset: const Offset(0, 2),
-            ),
-          ],
-        ),
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(16.0),
+      width: 1200,
+      height: 300,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(color: Colors.grey, width: 1.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 5.0,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text("About Us\n", 
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-              color: Color.fromARGB(255, 0, 0, 0)
-            )),
-            Text(
-              aboutUs,
-              style: const TextStyle(fontSize: 16)),
+          const Text("About Us\n",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Color.fromARGB(255, 0, 0, 0))),
+          Text(aboutUs, style: const TextStyle(fontSize: 16)),
         ],
       ),
     );
