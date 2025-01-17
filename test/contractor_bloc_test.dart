7dbc4720986e66c6ae5a7b842730dce7b65e2f13
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-01-17 17:18:01 -0600

    added bloc test for authentication and search

--- test/contractor_bloc_test.dart
+++ test/contractor_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rate_my_contractor/contractor_list/bloc/search_bloc.dart';
 import 'package:rate_my_contractor/contractor_list/domain/contractor_repository.dart';
@@ -18,5 +19,13 @@ void main() {
     test('initial state is ', () {
       expect(searchBloc.state, equals(const SearchState()));
     });
+
+    blocTest<SearchBloc, SearchState>(
+      'emits [query i] when SearchTextUpdated',
+      build: () => SearchBloc(mockRepository),
+      act: (bloc) => bloc.add(const SearchTextUpdated(query: 'i')),
+      expect: () =>
+          [const SearchState(query: 'i', status: SearchStateStatus.valid)],
+    );
   });
 }
