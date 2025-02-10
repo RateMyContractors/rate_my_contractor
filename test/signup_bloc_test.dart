bda5bc1494f69a4d36dbe49fa8e6f84b5d9fd981
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-02-10 15:16:58 -0600

    added more test to signup bloc

--- test/signup_bloc_test.dart
+++ test/signup_bloc_test.dart
@@ -3,6 +3,8 @@ import 'package:mocktail/mocktail.dart';
 import 'package:rate_my_contractor/authentication/domain/authentication_repository.dart';
 import 'package:rate_my_contractor/authentication/signup/bloc/signup_bloc.dart';
 import 'package:rate_my_contractor/authentication/signup/models/email.dart';
+import 'package:rate_my_contractor/authentication/signup/models/firstname.dart';
+import 'package:rate_my_contractor/authentication/signup/models/lastname.dart';
 import 'package:rate_my_contractor/authentication/signup/models/password.dart';
 import 'package:test/test.dart';
 
@@ -56,5 +58,78 @@ void main() {
         ),
       ],
     );
+
+    blocTest<SignUpBloc, SignUpState>(
+      'emits First name changed valdation',
+      build: () => SignUpBloc(authenticationRepository: mockRepository),
+      act: (bloc) => bloc.add(const SignUpFirstNameChanged('Bridget')),
+      expect: () => [
+        const SignUpState(
+          firstName: FirstName.dirty('Bridget'),
+        ),
+      ],
+    );
+
+    blocTest<SignUpBloc, SignUpState>(
+      'emits Last name changed valdation',
+      build: () => SignUpBloc(authenticationRepository: mockRepository),
+      act: (bloc) => bloc.add(const SignUpLastNameChanged('last')),
+      expect: () => [
+        const SignUpState(
+          lastName: LastName.dirty('last'),
+        ),
+      ],
+    );
   });
 }
+/*
+
+  FutureOr<void> _onUsernameChanged(
+    SignUpUsernameChanged event,
+    Emitter<SignUpState> emit,
+  ) {
+    final username = Username.dirty(event.username);
+    emit(
+      state.copyWith(
+        username: username,
+        isValid: Formz.validate([
+          username,
+          state.lastName,
+          state.firstName,
+          state.password,
+          state.email,
+        ]),
+      ),
+    );
+  }
+
+  FutureOr<void> _onSubmitted(
+    SignUpSubmitted event,
+    Emitter<SignUpState> emit,
+  ) async {
+    if (state.isValid) {
+      emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
+      try {
+        await _authenticationRepository.signUp(
+          state.email.value,
+          state.password.value,
+          state.username.value,
+          state.firstName.value,
+          state.lastName.value,
+          state.userType,
+        );
+        emit(state.copyWith(status: FormzSubmissionStatus.success));
+      } on Exception catch (_) {
+        emit(state.copyWith(status: FormzSubmissionStatus.failure));
+      }
+    }
+  }
+
+  FutureOr<void> _onUserTypeChanged(
+    SignUpUserType event,
+    Emitter<SignUpState> emit,
+  ) {
+    emit(state.copyWith(userType: event.userType));
+  }
+}
+*/
