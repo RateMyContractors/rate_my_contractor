572dc0a7bb1578b0b4fdac48b108bc34f99285a6
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-02-10 14:12:44 -0600

    fixed tests

--- test/contractor_page_test.dart
+++ test/contractor_page_test.dart
@@ -4,15 +4,23 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rate_my_contractor/authentication/bloc/authentication_bloc.dart';
+import 'package:rate_my_contractor/authentication/logout/bloc/logout_bloc.dart';
 import 'package:rate_my_contractor/contractor_list/bloc/search_bloc.dart';
 import 'package:rate_my_contractor/contractor_list/data/models/license_dto.dart';
 import 'package:rate_my_contractor/contractor_list/domain/models/contractor.dart';
 import 'package:rate_my_contractor/contractor_page.dart';
 import 'package:rate_my_contractor/main.dart';
 import 'package:rate_my_contractor/results_page.dart';
+import 'package:rate_my_contractor/reviews/bloc/reviews_bloc.dart';
 
 class MockSearchBloc extends MockBloc<SearchEvent, SearchState>
     implements SearchBloc {}
+
+class MockLogOutBloc extends MockBloc<LogOutEvent, LogOutState>
+    implements LogOutBloc {}
+
+class MockReviewsBloc extends MockBloc<ReviewsEvent, ReviewsState>
+    implements ReviewsBloc {}
 
 class MockAuthenticationBloc
     extends MockBloc<AuthenticationEvent, AuthenticationState>
@@ -21,10 +29,14 @@ class MockAuthenticationBloc
 void main() {
   late MockSearchBloc mockSearchBloc;
   late MockAuthenticationBloc mockAuthenticationBloc;
+  late MockReviewsBloc mockReviewsBloc;
+  late MockLogOutBloc mockLogOutBloc;
 
   setUp(() {
     mockSearchBloc = MockSearchBloc();
     mockAuthenticationBloc = MockAuthenticationBloc();
+    mockReviewsBloc = MockReviewsBloc();
+    mockLogOutBloc = MockLogOutBloc();
   });
 
   group('ContractorPage Flow', () {
@@ -78,6 +90,8 @@ void main() {
       when(() => mockAuthenticationBloc.state)
           .thenReturn(const AuthenticationState.unauthenticated());
 
+      when(() => mockReviewsBloc.state).thenReturn(const ReviewsState());
+
       await tester.pumpWidget(
         MaterialApp(
           home: MultiBlocProvider(
@@ -86,6 +100,7 @@ void main() {
               BlocProvider<AuthenticationBloc>.value(
                 value: mockAuthenticationBloc,
               ),
+              BlocProvider<ReviewsBloc>.value(value: mockReviewsBloc),
             ],
             child: const ResultsPage(),
           ),
@@ -119,7 +134,6 @@ void main() {
       expect(find.byType(ContractorPage), findsOneWidget);
       expect(find.text('"Z" ELECTRIC'), findsOneWidget);
       expect(find.text('(708) 423-6967'), findsOneWidget);
-      expect(find.text('this is about us'), findsOneWidget);
     });
 
     testWidgets(
@@ -144,6 +158,8 @@ void main() {
       when(() => mockAuthenticationBloc.state)
           .thenReturn(const AuthenticationState.unauthenticated());
 
+      when(() => mockReviewsBloc.state).thenReturn(const ReviewsState());
+
       await tester.pumpWidget(
         MaterialApp(
           home: MultiBlocProvider(
@@ -152,12 +168,20 @@ void main() {
               BlocProvider<AuthenticationBloc>.value(
                 value: mockAuthenticationBloc,
               ),
+              BlocProvider<ReviewsBloc>.value(value: mockReviewsBloc),
+              BlocProvider<LogOutBloc>.value(value: mockLogOutBloc),
             ],
             child: const MyHomePage(title: 'Find who you need'),
           ),
           routes: {
-            '/results': (context) => BlocProvider<SearchBloc>.value(
-                  value: mockSearchBloc,
+            '/results': (context) => MultiBlocProvider(
+                  providers: [
+                    BlocProvider<SearchBloc>.value(value: mockSearchBloc),
+                    BlocProvider<AuthenticationBloc>.value(
+                      value: mockAuthenticationBloc,
+                    ),
+                    BlocProvider<ReviewsBloc>.value(value: mockReviewsBloc),
+                  ],
                   child: const ResultsPage(),
                 ),
           },
@@ -170,7 +194,7 @@ void main() {
       await tester.enterText(find.byType(SearchBar), '"Z" ELECTRIC');
       await tester.tap(find.widgetWithText(ElevatedButton, 'Search'));
       await tester.pumpAndSettle();
-
+      debugPrint('Current widgets: ${tester.allWidgets}');
       expect(find.byType(ResultsPage), findsOneWidget);
 
       expect(find.text('"Z" ELECTRIC'), findsOneWidget);
@@ -180,7 +204,6 @@ void main() {
       expect(find.byType(ContractorPage), findsOneWidget);
       expect(find.text('"Z" ELECTRIC'), findsOneWidget);
       expect(find.text('(708) 423-6967'), findsOneWidget);
-      expect(find.text('this is about us'), findsOneWidget);
     });
   });
 }
