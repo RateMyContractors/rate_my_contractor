a43b93c3e8fb1cf9d22c9845b8c28e321e0c7b7d
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-02-10 14:35:33 -0600

    added test for review bloc

---
+++ test/reviews_bloc_test.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rate_my_contractor/reviews/bloc/reviews_bloc.dart';
+import 'package:rate_my_contractor/reviews/domain/reviews_repository.dart';
+
+class MockReviewsBlocRepository extends Mock implements ReviewsRepository {}
+
+void main() {
+  group(ReviewsBloc, () {
+    late ReviewsBloc reviewsBloc;
+    late MockReviewsBlocRepository mockRepository;
+
+    setUp(() {
+      mockRepository = MockReviewsBlocRepository();
+      reviewsBloc = ReviewsBloc(mockRepository);
+    });
+
+    test('initial state is ', () {
+      expect(reviewsBloc.state, equals(const ReviewsState()));
+    });
+
+    blocTest<ReviewsBloc, ReviewsState>(
+      'emits comment when reviews comment changed',
+      build: () => ReviewsBloc(mockRepository),
+      act: (bloc) => bloc.add(const ReviewsCommentChanged(comment: 'hey')),
+      expect: () => [const ReviewsState(comment: 'hey')],
+    );
+
+    blocTest<ReviewsBloc, ReviewsState>(
+      'emits star rating when start is pressed',
+      build: () => ReviewsBloc(mockRepository),
+      act: (bloc) => bloc.add(const ReviewsStarsPressed(starRating: 5)),
+      expect: () =>
+          [const ReviewsState(rating: 5, status: ReviewsStateStatus.valid)],
+    );
+  });
+}
