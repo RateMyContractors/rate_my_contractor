c0193c2cf510c473b70d76ac16eac95e8a905961
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-02-10 20:44:18 -0600

    added a review form test

--- lib/reviews/screens/leaving_reviews_page.dart
+++ lib/reviews/screens/leaving_reviews_page.dart
@@ -82,118 +82,163 @@ class ReviewForm extends StatelessWidget {
       builder: (context, state) {
         return Center(
           child: Container(
-            // margin: const EdgeInsets.only(bottom: 70, top: 70),
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: MediaQuery.of(context).size.height * 0.95,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey),
-              boxShadow: const [
-                BoxShadow(
-                  blurRadius: 5,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            //width: MediaQuery.of(context).size.width * 0.7,
-            child: Column(
-              children: [
-                const SizedBox(height: 15),
-                const Text(
-                  'Create Review',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                ),
-                const SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.only(left: 16),
-                  child: Align(
-                    alignment: Alignment.centerLeft, // Align text to the left
-                    child: Text(
-                      companyName,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.primary,
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.95,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: Colors.grey),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              //width: MediaQuery.of(context).size.width * 0.7,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    const Text(
+                      'Create Review',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    const SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: Align(
+                        alignment:
+                            Alignment.centerLeft, // Align text to the left
+                        child: Text(
+                          companyName,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                const Divider(height: 10),
-                const SizedBox(height: 15),
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Align(
-                    alignment: Alignment.centerLeft, // Align text to the left
-                    child: Text(
-                      'Overall Rating',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                    const SizedBox(height: 15),
+                    const Divider(height: 10),
+                    const SizedBox(height: 15),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Align(
+                        alignment:
+                            Alignment.centerLeft, // Align text to the left
+                        child: Text(
+                          'Overall Rating',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                StarSytem(star: state.rating),
-                const SizedBox(height: 15),
-                const Divider(height: 10),
-                const SizedBox(height: 15),
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Align(
-                    alignment: Alignment.centerLeft, // Align text to the left
-                    child: Text(
-                      'Add a written review',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                    const SizedBox(height: 15),
+                    StarSytem(star: state.rating),
+                    const SizedBox(height: 15),
+                    const Divider(height: 10),
+                    const SizedBox(height: 15),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Align(
+                        alignment:
+                            Alignment.centerLeft, // Align text to the left
+                        child: Text(
+                          'Add a written review',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                SizedBox(
-                  height: 150,
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  child: TextField(
-                    maxLines: null,
-                    expands: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'What do you think of the company?',
-                    ),
-                    onChanged: (comment) {
-                      context
-                          .read<ReviewsBloc>()
-                          .add(ReviewsCommentChanged(comment: comment));
-                    },
-                  ),
-                ),
-                const SizedBox(height: 15),
-                const Divider(height: 10),
-                const SizedBox(height: 15),
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Align(
-                    alignment: Alignment.centerLeft, // Align text to the left
-                    child: Text(
-                      'Add a photo',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      height: 150,
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: TextField(
+                        maxLines: null,
+                        expands: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'What do you think of the company?',
+                        ),
+                        onChanged: (comment) {
+                          context
+                              .read<ReviewsBloc>()
+                              .add(ReviewsCommentChanged(comment: comment));
+                        },
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                SizedBox(
-                  child: size.width < 800
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Column(
+                    const SizedBox(height: 15),
+                    const Divider(height: 10),
+                    const SizedBox(height: 15),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Align(
+                        alignment:
+                            Alignment.centerLeft, // Align text to the left
+                        child: Text(
+                          'Add a photo',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      child: size.width < 800
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        ImagePicker().pickImage(
+                                            source: ImageSource.gallery);
+                                      },
+                                      icon: Icon(
+                                        Icons.upload,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        size: 50,
+                                        semanticLabel: 'Insert Photo',
+                                      ),
+                                    ),
+                                    const Text('Upload'),
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        ImagePicker().pickImage(
+                                            source: ImageSource.camera);
+                                      },
+                                      icon: Icon(
+                                        Icons.camera_alt,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        size: 50,
+                                        semanticLabel: 'take_a_picture',
+                                      ),
+                                    ),
+                                    const Text('Take a picture'),
+                                  ],
+                                ),
+                              ],
+                            )
+                          : Column(
                               children: [
                                 IconButton(
                                   onPressed: () {
@@ -211,90 +256,53 @@ class ReviewForm extends StatelessWidget {
                                 const Text('Upload'),
                               ],
                             ),
-                            Column(
-                              children: [
-                                IconButton(
-                                  onPressed: () {
-                                    ImagePicker()
-                                        .pickImage(source: ImageSource.camera);
-                                  },
-                                  icon: Icon(
-                                    Icons.camera_alt,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                    size: 50,
-                                    semanticLabel: 'take_a_picture',
-                                  ),
+                    ),
+                    const SizedBox(height: 15),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.primary,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              'Cancel',
+                              style: Theme.of(context).textTheme.displaySmall,
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          ElevatedButton(
+                            onPressed: () {
+                              BlocProvider.of<ReviewsBloc>(context).add(
+                                ReviewsFormButtonPressed(
+                                  contractorid: contractorid,
+                                  reviewerid: userid,
+                                  rating: state.rating,
+                                  comment: state.comment,
+                                  upvote: 1,
+                                  downvote: 1,
+                                  username: username,
+                                  usertype: usertype,
                                 ),
-                                const Text('Take a picture'),
-                              ],
-                            ),
-                          ],
-                        )
-                      : Column(
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                ImagePicker()
-                                    .pickImage(source: ImageSource.gallery);
-                              },
-                              icon: Icon(
-                                Icons.upload,
-                                color: Theme.of(context).colorScheme.primary,
-                                size: 50,
-                                semanticLabel: 'Insert Photo',
-                              ),
-                            ),
-                            const Text('Upload'),
-                          ],
-                        ),
-                ),
-                const SizedBox(height: 15),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.primary,
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text(
-                          'Cancel',
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
+                              );
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Post',
+                                style: TextStyle(fontSize: 20)),
+                          ),
+                          const SizedBox(width: 8),
+                        ],
                       ),
-                      const SizedBox(width: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          BlocProvider.of<ReviewsBloc>(context).add(
-                            ReviewsFormButtonPressed(
-                              contractorid: contractorid,
-                              reviewerid: userid,
-                              rating: state.rating,
-                              comment: state.comment,
-                              upvote: 1,
-                              downvote: 1,
-                              username: username,
-                              usertype: usertype,
-                            ),
-                          );
-                          Navigator.pop(context);
-                        },
-                        child:
-                            const Text('Post', style: TextStyle(fontSize: 20)),
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              )),
         );
       },
     );
---
+++ test/review_form_test.dart
@@ -0,0 +1,145 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rate_my_contractor/authentication/bloc/authentication_bloc.dart';
+import 'package:rate_my_contractor/authentication/domain/authentication_repository.dart';
+import 'package:rate_my_contractor/authentication/login/models/user.dart';
+import 'package:rate_my_contractor/authentication/logout/bloc/logout_bloc.dart';
+import 'package:rate_my_contractor/contractor_list/bloc/search_bloc.dart';
+import 'package:rate_my_contractor/contractor_list/data/models/license_dto.dart';
+import 'package:rate_my_contractor/contractor_list/domain/models/contractor.dart';
+import 'package:rate_my_contractor/contractor_page.dart';
+import 'package:rate_my_contractor/results_page.dart';
+import 'package:rate_my_contractor/reviews/bloc/reviews_bloc.dart';
+
+class MockSearchBloc extends MockBloc<SearchEvent, SearchState>
+    implements SearchBloc {}
+
+class MockLogOutBloc extends MockBloc<LogOutEvent, LogOutState>
+    implements LogOutBloc {}
+
+class MockReviewsBloc extends MockBloc<ReviewsEvent, ReviewsState>
+    implements ReviewsBloc {}
+
+class MockAuthenticationBloc
+    extends MockBloc<AuthenticationEvent, AuthenticationState>
+    implements AuthenticationBloc {}
+
+void main() {
+  late MockSearchBloc mockSearchBloc;
+  late MockAuthenticationBloc mockAuthenticationBloc;
+  late MockReviewsBloc mockReviewsBloc;
+  late MockLogOutBloc mockLogOutBloc;
+
+  setUp(() {
+    mockSearchBloc = MockSearchBloc();
+    mockAuthenticationBloc = MockAuthenticationBloc();
+    mockReviewsBloc = MockReviewsBloc();
+    mockLogOutBloc = MockLogOutBloc();
+  });
+
+  group('Reviewform test', () {
+    testWidgets('Reviewform is present', (WidgetTester tester) async {
+      when(() => mockAuthenticationBloc.state).thenReturn(
+        const AuthenticationState.authenticated(
+          user: User(
+            userstatus: AuthenticationStatus.authenticated,
+            id: '1234',
+            email: '2233@gmail.com',
+            username: 'mmomma',
+            usertype: 'contractor',
+          ),
+        ),
+      );
+
+      when(() => mockReviewsBloc.state).thenReturn(const ReviewsState());
+
+      when(() => mockSearchBloc.state).thenReturn(
+        const SearchState(
+          status: SearchStateStatus.success,
+          contractors: [
+            Contractor(
+              id: '845',
+              companyName: '"Z" ELECTRIC',
+              address: '10821 S KENTON AVE, OAK LAWN, IL 60453-',
+              tags: ['Electrical Contractor (General)'],
+              phone: '(708) 423-6967',
+              licenses: [
+                LicenseDto(
+                  licenseNumber: 'ECC92504',
+                  licenseType: 'Electrical Contractor (General)',
+                  town: 'Oak Lawn',
+                  id: 'a629e63c-4c7b-4991-b2da-7ccb37c77cec',
+                  licenseExpiration: '2021-12-15',
+                  isActive: 'true',
+                  contractorId: '845',
+                ),
+              ],
+              totalRating: [],
+            ),
+            Contractor(
+              id: '123',
+              companyName: 'ELIZABETH',
+              address: '94 JOHN ST, HINDSDALE',
+              tags: ['Electrical Contractor (General)'],
+              phone: '(520)-956-1258',
+              licenses: [
+                LicenseDto(
+                  licenseNumber: 'ECC92504',
+                  licenseType: 'Electrical Contractor (General)',
+                  town: 'Hindsdale',
+                  id: 'a6c',
+                  licenseExpiration: '2021-11-16',
+                  isActive: 'true',
+                  contractorId: '123',
+                ),
+              ],
+              totalRating: [],
+            ),
+          ],
+        ),
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MultiBlocProvider(
+            providers: [
+              BlocProvider<SearchBloc>.value(value: mockSearchBloc),
+              BlocProvider<AuthenticationBloc>.value(
+                value: mockAuthenticationBloc,
+              ),
+              BlocProvider<ReviewsBloc>.value(value: mockReviewsBloc),
+            ],
+            child: const ResultsPage(),
+          ),
+          routes: {
+            '/contractor': (context) => ContractorPage(
+                  contractor:
+                      ModalRoute.of(context)!.settings.arguments! as Contractor,
+                ),
+          },
+        ),
+      );
+      expect(find.byType(ResultsPage), findsOneWidget);
+
+      await tester.enterText(find.byType(TextField), 'Z');
+
+      await tester.tap(
+        find
+            .descendant(
+              of: find.byType(ListView),
+              matching: find.text('"Z" ELECTRIC'),
+            )
+            .first,
+      );
+      await tester.pumpAndSettle();
+
+      expect(find.byType(ContractorPage), findsOneWidget);
+      await tester.scrollUntilVisible(find.text('Write a review'), 100);
+      await tester.tap(find.text('Write a review'));
+      await tester.pumpAndSettle();
+    });
+  });
+}
