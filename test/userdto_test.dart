2fa6b7a9db86ad066f1e087fc4a172f6c816a3be
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-02-26 20:40:10 -0600

    added a user model test

---
+++ test/userdto_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rate_my_contractor/authentication/login/models/user.dart';
+
+void main() {
+  group('testing user model', () {
+    test('testing toString', () {
+      const fakeUser = User(
+          id: 'test',
+          email: 'test',
+          firstname: 'test',
+          lastname: 'test',
+          username: 'test',
+          usertype: 'test');
+      expect(
+          fakeUser.toString(),
+          'User{status: AuthenticationStatus.unknown,id: test,email: test, '
+          'firstname: test,lastname: test, '
+          'username:test,usertype:test}');
+    });
+  });
+}
