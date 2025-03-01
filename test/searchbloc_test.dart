8652df6f287331448d72ed479a86c982e4b00811
Bridget Torres <67389317+bridgettorres@users.noreply.github.com>
2025-03-01 10:50:02 -0600

    added searchbloc test

--- test/searchbloc_test.dart
+++ test/searchbloc_test.dart
@@ -1,38 +1,97 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:rate_my_contractor/contractor_list/bloc/search_bloc.dart';
+import 'package:rate_my_contractor/contractor_list/domain/contractor_repository.dart';
+import 'package:rate_my_contractor/contractor_list/domain/models/contractor.dart';
 import 'package:test/test.dart';
 
+class MockSearchBlocRepository extends Mock implements ContractorRepository {}
+
 void main() {
-  test('Search State works successfully', () {
-    const searchstate = SearchState(query: 'i');
+  group(SearchBloc, () {
+    late MockSearchBlocRepository mockRepository;
 
-    expect(
-        searchstate.toString(),
-        'SearchState(isButtonOn: query: i, errormsg: '
-        ', '
-        'contractors: [], status: SearchStateStatus.initial)');
-  });
+    setUp(() {
+      mockRepository = MockSearchBlocRepository();
+    });
 
-  test('Search Event for SearchText updated', () {
-    const searchtext = SearchTextUpdated(query: 'i');
+    test('Search State works successfully', () {
+      const searchstate = SearchState(query: 'i');
 
-    expect(searchtext.toString(), 'Search Commenced {query: i}');
-  });
+      expect(
+          searchstate.toString(),
+          'SearchState(isButtonOn: query: i, errormsg: '
+          ', '
+          'contractors: [], status: SearchStateStatus.initial)');
+    });
 
-  test('Search Event for SearchFilterPressed', () {
-    const searchtext = SearchFilterPressed(filter: 2);
+    test('Search Event for SearchText updated', () {
+      const searchtext = SearchTextUpdated(query: 'i');
 
-    expect(searchtext.toString(), 'Search filtered {filter: 2}');
-  });
+      expect(searchtext.toString(), 'Search Commenced {query: i}');
+    });
 
-  test('Search Sort Pressed for Search event', () {
-    const searchtext = SearchSortPressed(sort: true);
+    test('Search Event for SearchFilterPressed', () {
+      const searchtext = SearchFilterPressed(filter: 2);
 
-    expect(searchtext.toString(), 'Search filtered {sort: true}');
-  });
+      expect(searchtext.toString(), 'Search filtered {filter: 2}');
+    });
 
-  test('SearchButton pressed', () {
-    const searchtext = SearchButtonPressed();
+    test('Search Sort Pressed for Search event', () {
+      const searchtext = SearchSortPressed(sort: true);
 
-    expect(searchtext.toString(), 'Search Button Pressed');
+      expect(searchtext.toString(), 'Search filtered {sort: true}');
+    });
+
+    test('SearchButton pressed', () {
+      const searchtext = SearchButtonPressed();
+
+      expect(searchtext.toString(), 'Search Button Pressed');
+    });
+
+    blocTest<SearchBloc, SearchState>(
+      'search button pressed',
+      build: () {
+        when(
+          () => mockRepository.getContractors(
+            any(),
+            sortcontractors: any(named: 'sortcontractors'),
+          ),
+        ).thenAnswer(
+          (_) async => <Contractor>[
+            const Contractor(
+              id: '1',
+              companyName: 'test',
+              address: 'test123',
+              tags: ['hi'],
+              phone: '123',
+              licenses: [],
+              totalRating: [],
+            ),
+          ],
+        );
+        return SearchBloc(mockRepository);
+      },
+      act: (bloc) => bloc.add(const SearchButtonPressed()),
+      expect: () => [
+        const SearchState(
+          status: SearchStateStatus.loading,
+        ),
+        const SearchState(
+          contractors: [
+            Contractor(
+              id: '1',
+              companyName: 'test',
+              address: 'test123',
+              tags: ['hi'],
+              phone: '123',
+              licenses: [],
+              totalRating: [],
+            ),
+          ],
+          status: SearchStateStatus.success,
+        ),
+      ],
+    );
   });
 }
